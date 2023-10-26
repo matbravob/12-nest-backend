ec6b9d61614b3913c9757fd7361ab60d2dad82ef
matbravob <matbravob@gmail.com>
2023-10-26 14:14:51 -0300

    1

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user-dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from './entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 import { Model } from 'mongoose';
+
+import * as bcryptjs from 'bcryptjs';
+
+import { JwtPayload } from './interfaces/jwt-payload';
+import { LoginResponse } from './interfaces/login-response';
+import { User } from './entities/user.entity';
+import { CreateUserDto, UpdateAuthDto, LoginDto, RegisterUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -11,20 +16,80 @@ export class AuthService {
   constructor(
     @InjectModel(User.name)
     private userModel: Model<User>,
+    private jwtService: JwtService,
 
     ) {}
 
-  create(createUserDto: CreateUserDto): Promise<User> {
 
-    console.log(createUserDto);
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    // console.log(createUserDto);
+    // const newUser = new this.userModel( createUserDto );
+    // return newUser.save();
 
-    const newUser = new this.userModel( createUserDto );
+    try {
+      const { password, ...userData } = createUserDto;
 
-    return newUser.save();
+      // 1- Encriptar la contraseña
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync( password, 10),
+        ...userData
+      });
+
+      // 2- Guardar el usuario
+      await newUser.save();
+      const { password:_, ...user } = newUser.toJSON()
+
+      return user;
+      
+    } 
+    catch (error) {
+      // console.log(error.code);
+      if( error.code === 11000 ){
+        throw new BadRequestException(`${ createUserDto.email } already exists!`)
+      }
+
+      throw new InternalServerErrorException('Something terrible happened!')
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async register( registerUserDto: RegisterUserDto): Promise<LoginResponse> {
+    
+    const user = await this.create( registerUserDto );
+    return {
+      user: user,
+      token: this.getJwtToken({ id: user._id })
+    } 
+  }
+
+  async login( loginDto: LoginDto): Promise<LoginResponse> {
+
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email });
+    if ( !user ) {
+      throw new UnauthorizedException('Not valid credentials - email');
+    }
+
+    if (!bcryptjs.compareSync( password, user.password ) ) {
+      throw new UnauthorizedException('Not valid credentials - password')
+    }
+
+    const { password:_, ...rest } = user.toJSON();
+
+    return {
+      user: rest,
+      token: this.getJwtToken({ id: user.id }),
+    };
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userModel.find();
+  }
+
+  async findUserById( id: string) {
+    const user = await this.userModel.findById( id );
+    const { password, ...rest } = user.toJSON();
+    return rest;
   }
 
   findOne(id: number) {
@@ -37,5 +102,11 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  // 3- Generar el JWT
+  getJwtToken( payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 }
--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -2,7 +2,7 @@ import { IsEmail, IsString, MinLength } from "class-validator";
 
 
 
-export class CreateUserDto {
+export class RegisterUserDto {
 
     @IsEmail()
     email: string;
@@ -12,6 +12,5 @@ export class CreateUserDto {
 
     @MinLength(6)
     password: string;
-
 
 }
